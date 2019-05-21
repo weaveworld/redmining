bc3957ee51987e9b7b361fb6b8d401a000098b08
weaveworld <43147365+weaveworld@users.noreply.github.com>
2019-05-21 12:44:34 +0200

    Update redmining.sh

--- redmining.sh
+++ redmining.sh
@@ -1,6 +1,6 @@
-cd $0/..
+cd "$( cd -P "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 export REDMINING_URL="https://redmine.example.com"
 export REDMINING_KEY="Your api key"
 export REDMINING_ID="Comma separated root ids"
-xdg-open http://localhost:3000/
+(sleep 0.5 && xdg-open http://localhost:3000/)&
 node .
